1e38a0c6e0e65098bd9410d51c934225e1c079df
Ahern_Li <402638794@qq.com>
2019-02-06 13:00:39 +0800

    添加列表页前端页面

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -5,4 +5,4 @@ from django.views.generic import View
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'blog/base.html', {})
+        return render(request, 'blog/blog_list.html', {})
